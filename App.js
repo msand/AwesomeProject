fe0e96874f9c9efbbb7bd3d61d57e6afcdfc2b65
Mikael Sand <msand@abo.fi>
2018-01-29 14:20:12 +0200

    Configure native builds and add example.
    Android - Upgrade compileSdkVersion, appcompat and gradle.
    iOS - Remove libRNSVG-tvOS.a from iOS build.

--- App.js
+++ App.js
@@ -5,33 +5,37 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import { Svg, Circle, Path } from 'react-native-svg';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+const SvgExample = () => (
+  <Svg width="200" height="200" viewBox="-100 -100 200 200">
+    <Path
+      fill="#F7931E"
+      stroke="#000"
+      strokeWidth="1"
+      d="M-100,0 L100,0z M0,-100 L0,100z"
+    />
+    <Circle cx="0" cy="0" r="10" fill="green" />
+  </Svg>
+);
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <SvgExample />
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
